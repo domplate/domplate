f5b3a8a39ab997d3954ae69f6651e3b11c03a041
Christoph Dorn <christoph@christophdorn.com>
2013-09-12 13:12:09 -0700

    run tests via nodejs

--- tests/simple.js
+++ tests/simple.js
@@ -1,34 +1,25 @@
 
-function dump(obj) { print(require('test/jsdump').jsDump.parse(obj)) };
 
+var ASSERT = require("assert");
 
-var ASSERT = require("test/assert");
-var UTIL = require("util");
+var DOMPLATE = require("../lib/domplate");
 
-var DOMPLATE = require("domplate", "domplate");
-
-
-exports.testHelloWorld = function () {
     
-    var rep;
+var rep;
 
-    with (DOMPLATE.tags) {
-        rep = DOMPLATE.domplate({
-            tag: DIV({"style": "color: red;"},"$object|capitalize"),
-            capitalize: function(str) {
-                return str.toUpperCase();
-            }
-        });
-    }    
-
-    var html = rep.tag.render({
-        object: "Hello World"
+with (DOMPLATE.tags) {
+    rep = DOMPLATE.domplate({
+        tag: DIV({"style": "color: red;"},"$object|capitalize"),
+        capitalize: function(str) {
+            return str.toUpperCase();
+        }
     });
-    
-    ASSERT.eq("<div style=\"color: red;\" class=\" \">HELLO WORLD</div>", html);
+}    
 
-};
+var html = rep.tag.render({
+    object: "Hello World"
+});
 
+ASSERT.equal("<div style=\"color: red;\" class=\" \">HELLO WORLD</div>", html);
 
-if (require.main === module.id)
-    require("os").exit(require("test/runner").run(exports));
+console.log("OK");
